a3f006be92fd60b72e3ae005342e0ebdcb461218
brandonOrellana <brandon.rojas.orellana@gmail.com>
2022-07-26 17:23:17 -0300

    inicio

---
+++ src/main/java/ar/com/ecommencer/controller/HelloController.java
@@ -0,0 +1,4 @@
+package ar.com.ecommencer.controller;
+
+public class HelloController {
+}
---
+++ src/test/java/ar/com/ecommencer/EcommencerApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ecommencer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommencerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
